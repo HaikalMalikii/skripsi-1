ac4aa59fbba38b082421901ce952837115565039
marcelramos <marcel.winarno@binus.ac.id>
2022-03-14 14:23:53 +0700

    Navbar

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -8,10 +8,29 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>HomePage</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content">
+          <a class="navbar-brand" href="#">
+              <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+          </a>
+      <a class="navbar-brand" href="#">Home</a>
+      <a class="navbar-brand" href="#">About</a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <form class="form-inline ">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+
+  </nav>
+
+
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -23,5 +42,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
+
   </body>
 </html>
