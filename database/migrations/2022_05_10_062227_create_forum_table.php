f0fde0c1296ef7f33e484fffa9588720518e9647
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-05-18 16:43:27 +0700

    forum waw

--- database/migrations/2022_05_10_062227_create_forum_table.php
+++ database/migrations/2022_05_10_062227_create_forum_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateForumTable extends Migration
 {
@@ -20,12 +21,10 @@ class CreateForumTable extends Migration
             $table->text("Aspirasi");
             $table->dateTime('Tanggal');
             $table->string('Photo_Forum');
-            $table->timestamps('created_at')->useCurrent();
-            $table->timestamps('updated_at')->useCurrent();
+            $table->timestamp('created_at')->Nullable()->useCurrent();
+            $table->timestamp('updated_at')->Nullable()->useCurrent();
 
-            $table->integer('total');
-
-            $table->timestamps();
+            //$table->integer('total');
         });
     }
 
