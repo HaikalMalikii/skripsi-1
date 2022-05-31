2d39407301c5bd611498b03662f075ffca799506
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-05-31 18:09:08 +0700

    edit dikit

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -7,20 +7,11 @@
         <h1 class="text-center">Welcome Admin</h1>
 
         <div class="form-row">
-            <div class="my-3 col-6">
-                <a class="btn btn-block btn-dark p-3" href="/admin-category">Category</a>
-            </div>
-            <div class="my-3 col-6">
-                <a class="btn btn-block btn-dark p-3" href="/admin-products">Products</a>
-            </div>
-            <div class="my-3 col-6">
-                <a class="btn btn-block btn-dark p-3" href="/admin-item-assigned">Item Assigned</a>
-            </div>
-            <div class="my-3 col-6">
-                <a class="btn btn-block btn-dark p-3" href="/admin-users">Users</a>
+            <div class="my-3 col-12">
+                <a class="btn btn-block btn-dark p-3" href="/admin-status">STATUS</a>
             </div>
             <div class="my-3 col-12">
-                <a class="btn btn-block btn-dark p-3" href="/admin-transactions">Transactions</a>
+                <a class="btn btn-block btn-dark p-3" href="/admin-berita">NEWS</a>
             </div>
         </div>
 
