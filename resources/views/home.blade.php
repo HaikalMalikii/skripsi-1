16d428a980c4e143b904ca45dc3fdd5250fc1742
nadhifa14 <nadhifaqila@gmail.com>
2022-06-16 21:41:02 +0700

    edit home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,108 +3,85 @@
 @section('content')
 
 <head>
+<style>
+    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+    .row.content {height: 1500px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height: auto;} 
+    }
+  </style>
 </head>
 <body>
-    <div class="jumbotron ">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4">WELCOME TO LAPORIN</h1>
+
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-9"> 
+      <h1 class="display-4">WELCOME TO LAPORIN</h1>
           <p class="lead my-3">menyediakan berbagai kemudahan bagi anda untuk berinetraksi dan menyampaikan keluhan</p>
           <button class="btn btn-primary btn-lg" type="button"><img src="css/foto/lapor.png" alt="lapor">Lapor</button>
           <button class="btn btn-primary btn-lg" type="button"><img src="css/foto/forum.png" alt="lapor">Forum</button>
           <button class="btn btn-primary btn-lg" type="button"><img src="css/foto/news.png" alt="lapor">Berita</button>
+    </div>
+    <div class="col-sm-3 sidenav">
+      <div class="well">
+        <h4 class="text-center">FORUM MASYARAKAT</h4>
+      </div>
+      <div class="card text-center" style="width: 100%;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-
-
-
-    <div class=" body d-flex flex-column">
-    <div class="d-flex justify-content-md-between">
-        <div class="col-sm-6">
-            <div class="row">
-                <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                    <h5 class="card-title">Aspirasi Rakyat</h5>
-                    <p class="card-text">Mayarakat dapat menuliskan keluhan nya di sini.</p>
-                   
-                    </div>
-                </div>
-                </div>
-                <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                    <h5 class="card-title">Forum Rakyat</h5>
-                    <p class="card-text">Masyarakat dapat melihat forum keluhan nya disini.</p>
-                   
-                    </div>
-                </div>
-                </div>
-            </div>
+      
+      <div class="card text-center" style="width: 100%;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-        <div class="mr-3">
-            <h4 class="text-center">FORUM MASYARAKAT</h4>
-            <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card text-center" style="width: 18rem;">
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+      </div>
+      <div class="card text-center" style="width: 100%;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+      </div>
     </div>
+  </div>
+</div>
 
-    <h4 class="text-center mt-3">BERITA TERKINI</h4>
-    <div class="d-flex justify-content-center">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h4 class="text-center mt-3">BERITA TERKINI</h4>
+      <div class="d-flex justify-content-center">
+          <div class="card" style="width: 20rem;">
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
-          </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-    </div>
+      </div>
 </div>
 </body>
 @endsection
