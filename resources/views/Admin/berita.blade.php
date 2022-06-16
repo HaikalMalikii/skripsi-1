841a0cdfc89b3c7a2de5550240770f0e112ae4d5
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-06-16 13:10:56 +0700

    test

--- resources/views/Admin/berita.blade.php
+++ resources/views/Admin/berita.blade.php
@@ -2,7 +2,6 @@
 
 @section('content');
 
-<<<<<<< HEAD
     <div class="container" style="padding-top: 7rem; min-height: 100vh">
         <h1>Berita</h1>
         @if ($errors->any())
@@ -17,48 +16,6 @@
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addBerita">
             Tulis Berita
         </button>
-=======
-<div class="container" style="padding-top: 7rem; min-height: 100vh">
-    <h1>Berita</h1>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addBerita">
-        Tulis Berita
-    </button>
-
-    <div class="modal fade bd-example-modal-xl" id="addBerita" tabindex="-1" aria-labelledby="addBeritaLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addBeritaLabel">Tulis Berita</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="/admin-add-berita" method="POST" enctype="multipart/form-data">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="judul">Judul Berita</label>
-                            <input placeholder="Judul" id="judul" type="text"
-                                class="form-control @error('judul') is-invalid @enderror" name="judul" required
-                                autocomplete="judul" autofocus>
-
-                            <!-- @error('judul')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror -->
-                        </div>
->>>>>>> 91290dd41866815e93e71efbce6ba35d1dc967f2
 
         <div class="modal fade bd-example-modal-xl" id="addBerita" tabindex="-1" aria-labelledby="addBeritaLabel"
             aria-hidden="true">
@@ -80,7 +37,6 @@
                                     class="form-control @error('judul') is-invalid @enderror" name="judul" required
                                     autocomplete="judul" autofocus>
 
-<<<<<<< HEAD
                                 @error('judul')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -114,22 +70,6 @@
                                 @enderror
                             </div>
                     </div>
-=======
-                            <div class="form-group">
-                              <label for="image">Image</label>
-                            {{-- <input placeholder="Image" id="image" type="file"
-                                class="form-control-file @error('image') is-invalid @enderror" name="image" required
-                                autocomplete="image" autofocus> --}}
-                                <input type="file" class="form-control-file" id="AddAduanGambarID" name="image">
-
-                              @error('image')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                            </div>
-                        </div>
->>>>>>> 91290dd41866815e93e71efbce6ba35d1dc967f2
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -162,7 +102,7 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="updateBeritaLabel{{ $b->id }}Label">
-                                                Berita {{ $b->judul }}</h5>
+                                                Judul Berita {{ $b->judul }}</h5>
                                             <button type="button" class="close" data-dismiss="modal"
                                                 aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
@@ -259,22 +199,5 @@
             </div>
         @endforeach
     </div>
-<<<<<<< HEAD
-=======
-    @foreach ($berita as $b )
-    <div class="card w-90">
-  <div class="card-body">
-    <td>
-        <img class="img-fluid" src="{{ asset("css/foto/$b->image") }}" alt="">
-    </td>
-   <a class="card-title" href="/Berita/{{$b->id}}">{{ $b->judul }}</a>
-    <p class="card-text">{{ $b->description}}</p>
-    <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#editBerita-{{ $b->id }}">Edit</button>
-    <button href="#" class="btn btn-warning btn-sm">Delete </button>
-  </div>
-</div>
-@endforeach
-</div>
->>>>>>> 91290dd41866815e93e71efbce6ba35d1dc967f2
 
 @endsection
