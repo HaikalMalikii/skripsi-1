f013591a44d8973311a7366a2dd0ec9ddfc4c9ba
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-06-20 11:02:08 +0700

    validasi forum dan comment kalo belom login

--- resources/views/Forum/index.blade.php
+++ resources/views/Forum/index.blade.php
@@ -1,75 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet"  href="{{asset('css/home.css')}}">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
-    <title>Add Forum</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Mono:ital,wght@0,400;1,500&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Condensed&family=Roboto+Mono:ital,wght@0,400;1,500&display=swap');
-        h3{
-            font-family: 'Bebas Neue', cursive;
-        }
-        .btn{
-        background-color: #FFE3A9;
-        color: black;
-        border-color: grey;
-        border-width: 2px;
-        border-radius: 10px;
-        font-family: 'Roboto Mono', monospace;
-    }
-    th{
-        font-family: 'Roboto Condensed', sans-serif;
-    }
-    </style>
-</head>
+        <title>Add Forum</title>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Mono:ital,wght@0,400;1,500&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Condensed&family=Roboto+Mono:ital,wght@0,400;1,500&display=swap');
 
-<body>
-<div class="main">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Forum Masyarkat</h3>
-                        <div class="right">
-                            <a href="{{ url('/addforum') }}" class="btn btn-sm btn-primary">New Forum</a>
+            h3 {
+                font-family: 'Bebas Neue', cursive;
+            }
+
+            .btn {
+                background-color: #FFE3A9;
+                color: black;
+                border-color: grey;
+                border-width: 2px;
+                border-radius: 10px;
+                font-family: 'Roboto Mono', monospace;
+            }
+
+            th {
+                font-family: 'Roboto Condensed', sans-serif;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Forum Masyarkat</h3>
+                                <div class="right">
+                                    @guest
+                                        <a href="{{ url('login') }}" class="btn btn-sm btn-primary">New Forum</a>
+                                    @else
+                                        <a href="{{ url('/addforum') }}" class="btn btn-sm btn-primary">New Forum</a>
+                                    @endguest
+                                </div>
+                            </div>
+                            @foreach ($forum as $f)
+                                <div class="card w-90">
+                                    <div class="card-body">
+                                        <a href="/ForumDetail/{{ $f->id }}">{{ $f->Judul }}</a>
+                                        <p class="card-text">{{ $f->Deskripsi }}</p>
+                                        <img src="{{ asset("css/foto/$f->Gambar") }}" alt="" srcset="">
+
+                                        <a href="/ForumDetail/{{ $f->id }}"
+                                            class="btn btn-warning btn-sm">Comment</a>
+                                        <a href="/ForumDetail/{{ $f->id }}" class="btn btn-warning btn-sm">View </a>
+                                    </div>
+                                </div>
+                            @endforeach
+
                         </div>
                     </div>
-                    @foreach ($forum as $f )
-                    <div class="card w-90">
-                        <div class="card-body">
-                            <a href="/ForumDetail/{{$f->id}}">{{ $f->Judul }}</a>
-                            <p class="card-text">{{ $f->Deskripsi }}</p>
-                            <img src="{{asset("css/foto/$f->Gambar")}}" alt="" srcset="">
-
-                            <a target="_blank" class="btn btn-warning btn-sm">Comment</a>
-                            <a href="#" class="btn btn-warning btn-sm">View </a>
-                            <a href="#" class="btn btn-danger btn-sm delete">Dislike </a>
-                        </div>
-                    </div>
-                    @endforeach
-
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
 
-</body>
+    </body>
 
-</html>
-
+    </html>
 @endsection
-
