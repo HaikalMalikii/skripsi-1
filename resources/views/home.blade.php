f7bd3fb3b6d8f834693605cff3bcc5928ee832fe
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-06-24 15:28:41 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,9 +35,9 @@
             }
 
             /*
-                            .container-fluid {
-                                background-color: lightcoral;
-                            } */
+                                                                                        .container-fluid {
+                                                                                            background-color: lightcoral;
+                                                                                        } */
 
             h1 {
                 font-size: 4vw;
@@ -48,10 +48,15 @@
             }
 
             .col-sm-3 {
+                float: right;
                 background-color: white;
+                margin-top: 50px;
+                /* margin-left: 50px; */
+                /* margin-right: 50px; */
             }
 
-            .col-sm-8 {
+            .col-sm-7 {
+                float: left;
                 margin-top: 50px;
                 margin-left: 50px;
                 margin-right: 80px;
@@ -71,7 +76,7 @@
     <body class="home">
         <div class="container-fluid">
             <div class="row content">
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h1 class="display-4">WELCOME TO LAPORIN</h1>
                     <p class="lead my-3">menyediakan berbagai kemudahan bagi anda untuk berinetraksi dan menyampaikan
                         keluhan
@@ -79,7 +84,7 @@
                     <a class="btn btn-primary btn-lg" type="button" href="/AddAduan"><img src="css/foto/lapor.png"
                             alt="lapor">Lapor</a>
                     <!-- <a class="btn btn-primary btn-lg" type="button" href="/AduanViewUser/{{ Auth::user()->id }} "><img src="css/foto/lapor.png"
-                                            alt="lapor">List Aduan</a> -->
+                                                                                                        alt="lapor">List Aduan</a> -->
                     <a class="btn btn-primary btn-lg" type="button" href="/addforum"><img src="css/foto/forum.png"
                             alt="lapor">Forum</a>
                     <a class="btn btn-primary btn-lg" type="button" href="/berita"><img src="css/foto/news.png"
@@ -87,7 +92,7 @@
 
                 </div>
 
-                <div class="col-sm-3 sidenav">
+                <div class="col-sm-3">
                     <div class="well">
                         <h4 class="text-center">FORUM MASYARAKAT</h4>
                     </div>
@@ -96,7 +101,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">{{ $df->Judul }}</h5>
                                 <!-- <img style="" class="img img-fluid p-2 rounded-lg"
-                                                    src="{{ asset("css/foto/$df->Gambar") }}" alt=""> -->
+                                                                                                                src="{{ asset("css/foto/$df->Gambar") }}" alt=""> -->
                                 <p class="card-text">{{ $df->Deskripsi }}</p>
                                 <a href="/ForumDetail/{{ $df->id }}" class="btn btn-warning btn-sm">Check it out</a>
                             </div>
@@ -133,16 +138,16 @@
                         </div>
                         <br>
                         <!-- <div class="card" style="width: 20rem;">
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{ $b->judul }}</h5>
-                                            <h5 class="card-title">{{ $b->id }}</h5>
+                                                                                                    <div class="card-body">
+                                                                                                        <h5 class="card-title">{{ $b->judul }}</h5>
+                                                                                                        <h5 class="card-title">{{ $b->id }}</h5>
 
-                                            <img style="" class="img img-fluid p-2 rounded-lg"
-                                                src="{{ asset("css/foto/$b->image") }}" alt="">
-                                            <p class="card-text">{{ Illuminate\Support\Str::limit($b->description, 100) }}</p>
-                                            <a href="/detail-berita/{{ $b->id }}" class="btn btn-warning btn-sm">Lihat Detail Berita</a>
-                                        </div>
-                                    </div> -->
+                                                                                                        <img style="" class="img img-fluid p-2 rounded-lg"
+                                                                                                            src="{{ asset("css/foto/$b->image") }}" alt="">
+                                                                                                        <p class="card-text">{{ Illuminate\Support\Str::limit($b->description, 100) }}</p>
+                                                                                                        <a href="/detail-berita/{{ $b->id }}" class="btn btn-warning btn-sm">Lihat Detail Berita</a>
+                                                                                                    </div>
+                                                                                                </div> -->
                     @endforeach
                 </div>
 
