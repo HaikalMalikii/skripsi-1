390ff81e9a5045efd3896c9586a5b9fd323c742f
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-06-24 17:14:39 +0700

    descending paginate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,8 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        $berita = Berita::paginate(1, ['*'], 'berita');
+        // $berita = Berita::paginate(1, ['*'], 'berita');
+        $berita = DB::table('berita')->orderBy('created_at', 'desc')->paginate(1, ['*'], 'berita');
         $Forum = Forum::all();
         $detailforum = DetailForum::paginate(5, ['*'], 'detailforum');
         $id = Auth::id();
